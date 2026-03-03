b2589a015c28b21d60fb1b0f8be1dbb404d028e3
LAujust <loyalaujust@outlook.com>
2026-03-03 08:01:35 +0000

    full

--- code/update_data.py
+++ code/update_data.py
@@ -9,6 +9,7 @@ from astropy.time import Time
 from utils import *
 from coadd import *
 from photometry import *
+import subprocess
 
 FOLLOWUP_ROOT = Path(__file__).resolve().parents[1]
 OPTICAL_ROOT = Path(os.environ.get("FOLLOWUP_OPTICAL_DIR", str(Path.home() / "optical_data"))).expanduser()
@@ -34,3 +35,10 @@ show_shift(root=str(OPTICAL_ROOT), save_dir=str(RESULTS_ROOT), ploter='plotly')
 calculate_observation_stats()
 show_obs_pie()
 show_cumulative_observations()
+
+
+#Push to GitHub
+os.chdir('/home/liangrd/Follow_up')
+subprocess.run(['git', 'add', '.'])
+subprocess.run(['git', 'commit', '-m', f'Update data at {Time.now().iso}'])
+subprocess.run(['git', 'push'])
